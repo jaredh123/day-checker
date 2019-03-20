118f3bb38cc919ff9ea115b76242ab443381bb35
jaredh123 <jared.hanson12345@gmail.com>
2019-03-20 14:32:40 -0700

    Complete day checker logic

---
+++ js/scripts.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+  $("#run").submit(function(event) {
+    event.preventDefault();
+    $("#result").text(year + result);
+
+    var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    var week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+    var d = parseInt($("#day").val());
+    var m = parseInt($("#month").val());
+    var yr = parseInt($("#year").val());
+
+    var yrCount = (((yr - 1) % 4) * 365) + (parseInt((yr - 1) / 4) * 366);
+
+    var yrCount = function() {
+      var count = 0;
+      for (i = 1; i < yr; i++) {
+        if ((i % 4 === 0) && (i % 100 !== 0) || (i % 400 === 0)) {
+          count += 366;
+        }
+        else {
+          count += 365;
+        }
+      }
+      return count;
+    }
+
+    var mCount = function() {
+      var result = 0;
+      for (i = 0; i < (m - 1); i++) {
+        if ((yr % 4 === 0) && (yr % 100 !== 0) || (yr % 400 === 0)) {
+          month[1] = 29;
+        }
+        else {
+          month[1] = 28;
+        }
+        result += month[i];
+      }
+      return result;
+    }
+
+    var fullCount = function() {
+      if ((((yrCount() + mCount() + d) % 7) - 1) == (-1)) {
+        return 6;
+      }
+      else {
+        return (((yrCount() + mCount() + d) % 7) - 1);
+      }
+    };
+
+    var day = function() {
+      if (d > month[m - 1] || m > (month.length)) {
+        return "That date doesn't exist!";
+      }
+      else {
+        return week[fullCount()];
+      }
+    }
+
+    $("#result").text("The above date is a " + day() + ".");
+  });
+});
