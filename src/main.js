424d08a0aa35f5df4f4c1ddbca26e18d2c19534f
jaredh123 <jared.hanson12345@gmail.com>
2019-03-21 07:53:04 -0700

    Add webpacks, linter, karma config, .json, etc.

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// import './styles.css';
+// import $ from 'jquery';
+// import { Date } from './checker';
+//
+// $(document).ready(function() {
+//   $("#run").submit(function(event) {
+//     event.preventDefault();
+//
+//     var d = parseInt($("#day").val());
+//     var m = parseInt($("#month").val());
+//     var yr = parseInt($("#year").val());
+//
+//     $("#result").text(year + result);
+//   });
+// });
