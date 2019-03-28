d9866f4ffc69b345da9f2678bcbb67d60ecd3603
jaredh123 <jared.hanson12345@gmail.com>
2019-03-28 11:09:55 -0700

    Refactor code to separate front and backend js logic

--- src/checker.js
+++ src/checker.js
@@ -1,61 +1,55 @@
-// export class Date {
-//   constructor(d, m, yr) {
-//     this.d = d;
-//     this.m = m;
-//     this.yr = yr;
-//   }
-// }
-//
-// var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-// var week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-//
-//
-//
-// var yrCount = (((yr - 1) % 4) * 365) + (parseInt((yr - 1) / 4) * 366);
-//
-// var yrCount = function() {
-//   var count = 0;
-//   for (i = 1; i < yr; i++) {
-//     if ((i % 4 === 0) && (i % 100 !== 0) || (i % 400 === 0)) {
-//       count += 366;
-//     }
-//     else {
-//       count += 365;
-//     }
-//   }
-//   return count;
-// }
-//
-// var mCount = function() {
-//   var result = 0;
-//   for (i = 0; i < (m - 1); i++) {
-//     if ((yr % 4 === 0) && (yr % 100 !== 0) || (yr % 400 === 0)) {
-//       month[1] = 29;
-//     }
-//     else {
-//       month[1] = 28;
-//     }
-//     result += month[i];
-//   }
-//   return result;
-// }
-//
-// var fullCount = function() {
-//   if ((((yrCount() + mCount() + d) % 7) - 1) == (-1)) {
-//     return 6;
-//   }
-//   else {
-//     return (((yrCount() + mCount() + d) % 7) - 1);
-//   }
-// };
-//
-// var day = function() {
-//   if (d > month[m - 1] || m > (month.length)) {
-//     return "That date doesn't exist!";
-//   }
-//   else {
-//     return week[fullCount()];
-//   }
-// }
-//
-// $("#result").text("The above date is a " + day() + ".");
+export default class DayChecker {
+  constructor(d, m, yr) {
+    this.d = d;
+    this.m = m;
+    this.yr = yr;
+  }
+
+  yrCount(yr) {
+    let count = 0;
+    for (let i = 1; i < yr; i++) {
+      if ((i % 4 === 0) && (i % 100 !== 0) || (i % 400 === 0)) {
+        count += 366;
+      }
+      else {
+        count += 365;
+      }
+    }
+    return count;
+  }
+
+  mCount(m, yr) {
+    const month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let result = 0;
+    for (let i = 0; i < (m - 1); i++) {
+      if ((yr % 4 === 0) && (yr % 100 !== 0) || (yr % 400 === 0)) {
+        month[1] = 29;
+      }
+      else {
+        month[1] = 28;
+      }
+      result += month[i];
+    }
+    return result;
+  }
+
+  fullCount(d, mCount, yrCount) {
+    if ((((yrCount + mCount + d) % 7) - 1) == (-1)) {
+      return 6;
+    }
+    else {
+      return (((yrCount + mCount + d) % 7) - 1);
+    }
+  };
+
+  day(d, m, fullCount) {
+    const month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    const week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    if (d > month[m - 1] || d <= 0 || m > (month.length) || m <= 0) {
+      return "That date doesn't exist!";
+    }
+    else {
+      return week[fullCount];
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,15 +1,19 @@
-// import './styles.css';
-// import $ from 'jquery';
-// import { Date } from './checker';
-//
-// $(document).ready(function() {
-//   $("#run").submit(function(event) {
-//     event.preventDefault();
-//
-//     var d = parseInt($("#day").val());
-//     var m = parseInt($("#month").val());
-//     var yr = parseInt($("#year").val());
-//
-//     $("#result").text(year + result);
-//   });
-// });
+import $ from 'jquery';
+import DayChecker from './checker';
+
+$(document).ready(function() {
+  $("#run").submit(function(event) {
+    event.preventDefault();
+
+    let dayChecker = new DayChecker();
+    let d = parseInt($("#day").val());
+    let m = parseInt($("#month").val());
+    let yr = parseInt($("#year").val());
+    let yrCount = dayChecker.yrCount(yr);
+    let mCount = dayChecker.mCount(m, yr);
+    let fullCount = dayChecker.fullCount(d, mCount, yrCount);
+    let day = dayChecker.day(d, m, fullCount);
+
+    $("#result").text(`The above date is a ${day}.`);
+  });
+});
